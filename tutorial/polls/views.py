1bdc74ee07cc1568e68c56b3eda8d447b62fb0d8
LeeHanYeong <dev@azelf.com>
2017-09-29 11:51:37 +0900

    index view목표

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -2,4 +2,27 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world! You're at the polls index.")
+    """
+    1. 모든 Question을 출력하는 View(Controller)구현
+    context dict객체를 생성,
+        'questions'키에
+        모든 Question객체를 DB에서 가져온 QuerySet을 할당
+
+    render함수를 사용해서 'polls/index.html'을
+    context와 함께 rendering한 결과를 리턴
+
+    2. 템플릿 파일들이 있는 디렉토리를 settings.py에 설정
+        settings.py에 TEMPLATE_DIR를 지정
+        TEMPLATE = ...설정의 'DIRS'키를 갖는 리스트에 TEMPLATE_DIR추가
+
+    3. 템플릿 파일 생성, Question들을 출력
+        polls/index.html파일을 생성
+        해당 템플릿에 'questions'키로 전달된 QuerySet을 for loop하며
+        각 loop에 해당하는 Question객체의 title을 출력
+
+
+
+    :param request:
+    :return:
+    """
+    # return HttpResponse("Hello, world! You're at the polls index.")
